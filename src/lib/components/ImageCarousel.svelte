a2c3965c858a32e5c334942e4a75288455e39cb7
Tyler Collier <git@tylercollier.com>
2024-07-12 14:50:38 -0600

    First attempt with Embla Carousel. Needs prev and next buttons and to hook them up to API.

--- src/lib/components/ImageCarousel.svelte
+++ src/lib/components/ImageCarousel.svelte
@@ -2,6 +2,7 @@
 	import BiggerPicture from 'bigger-picture/svelte';
 	import { onMount } from 'svelte';
 	import 'bigger-picture/css';
+	import emblaCarouselSvelte from 'embla-carousel-svelte';
 
 	export let images;
 
@@ -30,17 +31,38 @@
 
 <div>
 	{#if images.length}
-		<a class="cursor-pointer" on:click={showBiggerPicture}>
-			<img class="w-[500px]" src={currentImage.MediaURL} title="Primary image for listing; click to enlarge" alt="alt" />
-		</a>
-		<div class="flex flex-wrap items-center w-[500px]">
-			{#each images as image}
-				<a class="cursor-pointer mr-1" on:click={() => currentImage = image}>
-					<img class="w-[96px]" src={image.MediaURL} />
-				</a>
-			{/each}
+		<div>
+			<a class="cursor-pointer" on:click={showBiggerPicture}>
+				<img class="w-[500px]" src={currentImage.MediaURL} title="Primary image for listing; click to enlarge"
+					 alt="alt" />
+			</a>
+		</div>
+		<div class="w-[500px] border border-solid border-black embla" use:emblaCarouselSvelte>
+			<div class="embla__container">
+				{#each images as image}
+					<a class="embla__slide" on:click={() => currentImage = image}>
+						<img class="w-[96px]" src={image.MediaURL} />
+					</a>
+				{/each}
+			</div>
 		</div>
 	{:else}
 		There are no images to display for this listing
 	{/if}
 </div>
+
+<style>
+	.embla {
+			overflow: hidden;
+	}
+
+	.embla__container {
+			display: flex;
+			@apply items-center;
+	}
+
+	.embla__slide {
+			flex: 0 0 100px;
+			min-width: 0;
+	}
+</style>
