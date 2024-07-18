2faae1b1e90778c35febdbd1355c6ba71f3ee560
Tyler Collier <git@tylercollier.com>
2024-07-18 15:22:18 -0600

    Whoops, don't hardcode corolla as the area

--- src/params/subdivision.js
+++ src/params/subdivision.js
@@ -1,5 +1,11 @@
 import { allowedSubdivisions } from '$lib/area';
 
-export function match(value) {
-	return allowedSubdivisions.corolla.find(x => x.slug === value);
+export function match(subdivisionParam) {
+	// TODO: I don't know how to get the area parameter name in this matcher. We only get the subdivision param. Thus, we
+	//   currently allow mismatches, e.g. as long as the subdivision exists by slug in one area, we're acting as if it
+	//   exists in all area. Ultimately need to fix this. One hacky idea would be to look at the URL here.
+
+	return Object.values(allowedSubdivisions).some(subdivisions => {
+		return subdivisions.find(x => x.slug === subdivisionParam);
+	});
 }
