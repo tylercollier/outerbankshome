dd1d547fe06d61e342efd7ca781298fff47c09d6
Tyler Collier <git@tylercollier.com>
2024-07-20 18:41:29 -0600

    Keep ability for vite to do static analysis

--- src/lib/category.js
+++ src/lib/category.js
@@ -213,8 +213,11 @@ export const clientSideLoad = async ({ data }) => {
 	if (data.hasProse) {
 		try {
 			const baseFileName = getCategoryProseBaseFileName(data.categoryParam);
-			const path = `./components/prose/area/${data.areaParam}/category/${baseFileName}.svelte`;
-			proseComponent = (await import(path)).default;
+			// Reminder: in the following import, we inline the path, rather than creating a variable. This is important for
+			// Vite's ability to do static analysis. If we use a variable, we'll see this warning:
+			//   The above dynamic import cannot be analyzed by Vite.
+			//   See https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations for supported dynamic import formats. If this is intended to be left as-is, you can use the /* @vite-ignore */ comment inside the import() call to suppress this warning.
+			proseComponent = (await import(`./components/prose/area/${data.areaParam}/category/${baseFileName}.svelte`)).default;
 		} catch (e) {
 			// Do nothing
 		}
