bd5ce235d9e3028c8ef060bf7aa44c1bfec4c185
Tyler Collier <git@tylercollier.com>
2024-09-14 09:57:47 -0700

    Fix title and H1 on category pages

--- src/lib/components/CategoryPage.svelte
+++ src/lib/components/CategoryPage.svelte
@@ -1,17 +1,16 @@
 <script>
-	import { getAreaNameFromParam } from '$lib/area';
+	import { getTitleTag, getH1 } from '$lib/category';
 	import AreaListingResultsPage from '$lib/components/AreaListingResultsPage.svelte';
 
 	export let data;
-	$: ({ areaParam, activeListings, soldListings, proseComponent } = data);
-	$: areaName = getAreaNameFromParam(areaParam);
+	$: ({ areaParam, activeListings, categoryParam, soldListings, proseComponent } = data);
 </script>
 
 <svelte:head>
-	<title>{areaName} NC homes, houses, real estate and beach houses for sale</title>
+	<title>{getTitleTag(areaParam, categoryParam)}</title>
 </svelte:head>
 
-<h1>{areaName} Homes, Houses & Beach Houses For Sale</h1>
+<h1>{getH1(areaParam, categoryParam)}</h1>
 
 <AreaListingResultsPage {areaParam} {activeListings} {soldListings}>
 	<svelte:fragment slot="bottomProse">
