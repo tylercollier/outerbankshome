76374bcabf7413138081246fdaf4dbdf79b50e26
Tyler Collier <git@tylercollier.com>
2024-10-24 23:17:22 -0700

    Make swipie. Needs TLC.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -12,7 +12,9 @@
 		<div class="absolute top-4 right-0 flex items-center gap-5 mr-6 mb-10">
 			<div class="text-xl md:text-2xl font-medium">800-647-1868</div>
 			<!-- On small screens, the section below should become a hamburger menu -->
-			<div class="hidden md:flex"><LinkButton>Search Listings</LinkButton></div>
+			<div class="hidden md:flex">
+				<LinkButton>Search Listings</LinkButton>
+			</div>
 			<div class="hidden lg:flex"><a href="/outer%20banks/real%20estate/sellers/" class="no-underline">Sellers</a></div>
 			<div class="hidden lg:flex"><a href="/outer%20banks/real%20estate/buyers/" class="no-underline">Buyers</a></div>
 			<div class="hidden lg:flex"><a href="/contact-info.asp" class="no-underline">Contact</a></div>
@@ -21,8 +23,8 @@
 		</div>
 		<div class="absolute bottom-1 -z-10 bg-[#041c48] w-full h-[43px]"></div>
 	</div>
-	<!-- On medium and smaller screens, the section below should become a scrollie menu like in Google maps -->
-	<div class="topnav hidden md:flex flex-wrap items-center justify-center gap-6 border-black border-solid border-x-0 border-t-0 border-b py-4 text-lg">
+	<div
+		class="topnav hidden sm:flex flex-wrap items-center justify-center gap-6 border-black border-solid border-x-0 border-t-0 border-b py-4 text-lg">
 		<div><a href={makeLink('corolla/')} title="Corolla Home Sales">Corolla</a></div>
 		<div><a href={makeLink('duck/')} title="Duck Home Sales">Duck</a></div>
 		<div><a href={makeLink('southern-shores/')} title="Southern Shores Home Sales">Southern Shores</a></div>
@@ -35,16 +37,51 @@
 		<div><a href="{makeLink('sellers/')}" title="Sellers">Sellers</a></div>
 		<div><a href={makeLink('buyers/')} title="Buyers">Buyers</a></div>
 	</div>
-	<!-- End section -->
+	<div class="scrolling-wrapper">
+		<div><a href={makeLink('corolla/')} title="Corolla Home Sales">Corolla</a></div>
+		<div><a href={makeLink('duck/')} title="Duck Home Sales">Duck</a></div>
+		<div><a href={makeLink('southern-shores/')} title="Southern Shores Home Sales">Southern Shores</a></div>
+		<div><a href={makeLink('KittyHawk/')} title="Kitty Hawk Home Sales">Kitty Hawk</a></div>
+		<div><a href={makeLink('KillDevilHills/')} title="Kill Devil Hills Home Sales">Kill Devil Hills</a></div>
+		<div><a href={makeLink('colington/')} title="Colington Home Sales">Colington</a></div>
+		<div><a href={makeLink('nags head/')} title="Nags Head Home Sales">Nags Head</a></div>
+		<div><a href={makeLink('manteo/')} title="Manteo Home Sales">Manteo</a></div>
+		<div><a href={makeLink('hatteras-island/')} title="Hatteras Island Home Sales">Hatteras</a></div>
+		<div><a href="{makeLink('sellers/')}" title="Sellers">Sellers</a></div>
+		<div><a href={makeLink('buyers/')} title="Buyers">Buyers</a></div>
+	</div>
 </header>
 
-<style>
-	.topnav a {
-		@apply text-darkblue;
-		text-decoration: none;
-		font-weight:600;
-	}
-	.topnav a:hover {
-		@apply text-linkblue;
-	}
+<style lang="scss">
+  .topnav a {
+    @apply text-darkblue;
+    text-decoration: none;
+    font-weight: 600;
+  }
+
+  .topnav a:hover {
+    @apply text-linkblue;
+  }
+
+  .scrolling-wrapper {
+    background: white;
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+    scrollbar-width: none;
+    gap: 6px;
+
+    div {
+      white-space: nowrap;
+			border: 1px silver solid;
+			border-radius: 20px;
+			@apply p-2;
+    }
+  }
+
+  @media screen and (width >= 640px) {
+    .scrolling-wrapper {
+      display: none;
+    }
+  }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,9 @@
 
 	<Footer />
 
-	<div class="md:hidden w-full p-4 bg-white p-3 text-center absolute bottom-0 text-2xl font-bold sticky">
-		<a href="tel:8006471868" class="no-underline"><span class="text-obxorange">Call Agent:</span> <span class="text-black">(800)647-1868</span></a>
+	<div class="md:hidden w-full p-4 bg-white text-center bottom-0 text-2xl font-bold fixed">
+		<a href="tel:8006471868" class="no-underline"><span class="text-obxorange">Call Agent:</span> <span
+			class="text-black">(800)647-1868</span></a>
 	</div>
 </div>
+
