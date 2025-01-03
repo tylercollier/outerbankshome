b21533af02ed6ed730ecb9ec5e587164778d4621
Tyler Collier <git@tylercollier.com>
2025-01-03 09:57:36 -0700

    Only show certain listings on homepage #15

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,6 +4,12 @@ import { allowedAreas, getCity } from '$lib/area';
 export const load = async () => {
 	const listings = await getSearchResultListings('Residential', queryBuilder => {
 		const cities = allowedAreas.map(areaParam => getCity(areaParam));
+		queryBuilder = queryBuilder
+			.where('DaysOnMarket', '>=', 0)
+			.where('DaysOnMarket', '<=', 3)
+			.where('PropertySubType', 'in', ['Single Family Residence'])
+			.where('Property.StandardStatus', '=', 'Active')
+		;
 		return queryBuilder.where('City', 'in', cities);
 	});
 
